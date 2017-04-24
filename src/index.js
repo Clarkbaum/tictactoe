e781be21161b6d53a9ce8d594340c071b2bdf404
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2017-04-24 17:26:03 -0500

    should work in theory but npm prompt not acting as expected

--- src/index.js
+++ src/index.js
@@ -11,18 +11,21 @@ var board = [['.', '.', '.'], ['.', '.', '.'], ['.', '.', '.']];
 
 prompt.start();
 console.log(printBoard(board))
-console.log("choose a place to move (x,y)");
-prompt.get(properties, function (err, result) {
-  if (err) { return onErr(err); }
-  console.log('ok! moving your place there:');
-  var i = result.location.split(',')[0]
-  var j = result.location.split(',')[1]
-  console.log('  Location: ' + result.location);
-  console.log('  i: ', i);
-  console.log('  j: ', j);
-  movePiece(i, j);
-  console.log(printBoard(board));
-});
+console.log("choose a place to move (x,y starting at 0)");
+while(!!!checkWin()){
+  prompt.get(properties, function (err, result) {
+    if (err) { return onErr(err); }
+    console.log('ok! moving your place there:');
+    var i = result.location.split(',')[0]
+    var j = result.location.split(',')[1]
+    console.log('  i: ', i);
+    console.log('  j: ', j);
+    movePiece(i, j);
+    console.log(printBoard(board));
+  });
+}
+
+console.log(checkWin())
 
 function onErr(err) {
   console.log(err);
@@ -42,4 +45,26 @@ function printBoard(board){
 
 function movePiece(i, j){
   board[i][j] = 'X';
+}
+
+function checkWin(){
+  if (board[0][0] === 'X' && board[0][1] === 'X' && board[0][2] === 'X') return 'X Won !'
+  if (board[1][0] === 'X' && board[1][1] === 'X' && board[1][2] === 'X') return 'X Won !'
+  if (board[2][0] === 'X' && board[2][1] === 'X' && board[2][2] === 'X') return 'X Won !'
+  if (board[0][0] === 'X' && board[1][0] === 'X' && board[2][0] === 'X') return 'X Won !'
+  if (board[0][1] === 'X' && board[1][1] === 'X' && board[2][1] === 'X') return 'X Won !'
+  if (board[0][2] === 'X' && board[1][2] === 'X' && board[2][2] === 'X') return 'X Won !'
+  if (board[0][0] === 'X' && board[1][1] === 'X' && board[2][2] === 'X') return 'X Won !'
+  if (board[0][2] === 'X' && board[1][1] === 'X' && board[2][0] === 'X') return 'X Won !'
+
+  if (board[0][0] === 'O' && board[0][1] === 'O' && board[0][2] === 'O') return 'O Won !'
+  if (board[1][0] === 'O' && board[1][1] === 'O' && board[1][2] === 'O') return 'O Won !'
+  if (board[2][0] === 'O' && board[2][1] === 'O' && board[2][2] === 'O') return 'O Won !'
+  if (board[0][0] === 'O' && board[1][0] === 'O' && board[2][0] === 'O') return 'O Won !'
+  if (board[0][1] === 'O' && board[1][1] === 'O' && board[2][1] === 'O') return 'O Won !'
+  if (board[0][2] === 'O' && board[1][2] === 'O' && board[2][2] === 'O') return 'O Won !'
+  if (board[0][0] === 'O' && board[1][1] === 'O' && board[2][2] === 'O') return 'O Won !'
+  if (board[0][2] === 'O' && board[1][1] === 'O' && board[2][0] === 'O') return 'O Won !'
+
+  return false;
 }
