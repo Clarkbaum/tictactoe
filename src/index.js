9b80d5ef3cdde7980f24031fe545a7f1be917459
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2017-04-24 16:59:00 -0500

    got prompt doing what i want it

--- src/index.js
+++ src/index.js
@@ -2,13 +2,8 @@ var prompt = require('prompt');
 
 var properties = [
   {
-    name: 'username', 
-    validator: /^[a-zA-Z\s\-]+$/,
-    warning: 'Username must be only letters, spaces, or dashes'
-  },
-  {
-    name: 'password',
-    hidden: true
+    name: 'location', 
+    warning: 'move already taken'
   }
 ];
 
@@ -20,8 +15,7 @@ console.log("choose a place to move");
 prompt.get(properties, function (err, result) {
   if (err) { return onErr(err); }
   console.log('Command-line input received:');
-  console.log('  Username: ' + result.username);
-  console.log('  Password: ' + result.password);
+  console.log('  Location: ' + result.location);
 });
 
 function onErr(err) {
